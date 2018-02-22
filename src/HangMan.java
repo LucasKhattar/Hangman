fcd169f38a754f7c856223f014ae1991fe420f03
Samuel Sharp <league@WTS13.attlocal.net>
2018-02-21 18:26:01 -0800

    Day 2
    I got the random words to show up, next I will get the JOptionPane to
    show up.

--- src/HangMan.java
+++ src/HangMan.java
@@ -1,7 +1,12 @@
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Stack;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -14,12 +19,15 @@ public class HangMan implements KeyListener {
 	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
 	int total;
+	
+	Stack<String> words = new Stack<String>();
+
 	public static void main(String[] args) {
 		HangMan hang = new HangMan();
 		hang.runner();
 	}
+
 	ArrayList<Integer> randNums = new ArrayList<Integer>();
-	
 
 	void runner() {
 
@@ -28,13 +36,29 @@ public class HangMan implements KeyListener {
 		frame.setVisible(true);
 		frame.setSize(500, 50);
 		frame.addKeyListener(this);
-		String idk = JOptionPane.showInputDialog(null, "Gimme A Number");
-		int total = Integer.parseInt(idk);
-		JOptionPane.showMessageDialog(null, "Loading "+total+" Words. Good Luck");
+		String kik = JOptionPane.showInputDialog(null, "Gimme A Number");
+		int total = Integer.parseInt(kik);
+		JOptionPane.showMessageDialog(null, "Loading " + total + " Words, Good Luck");
 		for (int i = 0; i < total; i++) {
-			int rand = new Random().nextInt(3000);
-			if(randNums.contains(i)) {
-				i--;
+			try {
+				int counter = 0;
+				int rand = new Random().nextInt(3000);
+				BufferedReader br = new BufferedReader(new FileReader("src/dictionary.txt"));
+				String word = br.readLine();
+				while (word != null) {
+					if(counter == rand) {
+						words.push(word);
+					}
+					counter++;
+					word = br.readLine();
+				}
+				System.out.println(words);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			}
 		}
 	}
@@ -48,7 +72,7 @@ public class HangMan implements KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-	
+
 	}
 
 	@Override
